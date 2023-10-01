7d60913af2481dc04dbf77ef5f1f130d2b9be0ed
rupokghosh <rupokghosh291@gmail.com>
2023-09-30 17:45:37 -0700

    rewrite function using async & await

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const img = document.querySelector('img');
+const refreshBtn = document.getElementById('refreshBtn');
 
-let data = fetch('https://api.giphy.com/v1/gifs/translate?api_key=EvM6VPeOTodv8jPWvOvyEFdRCYeU21da&s=cats', {mode: 'cors'});
+// function that fetches the GIF
 
-data.then((response) => {
+async function getCatGif(){
 
-    return response.json();
-}).then((response) => {
-    img.src = response.data.images.original.url;
+   const response =  await fetch('https://api.giphy.com/v1/gifs/translate?api_key=EvM6VPeOTodv8jPWvOvyEFdRCYeU21da&s=cats', {mode: 'cors'});
+   const catData = await response.json();
+   img.src = catData.data.images.original.url;
+
+}
+getCatGif();
+
+// function for refresh button
+
+refreshBtn.addEventListener('click', () =>{
+    getCatGif();
 })
+
